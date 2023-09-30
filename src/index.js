c849326439b739a2d7d9d3873e1329f6f236a46b
luis miranda <luispsmiranda.96@gmail.com>
2023-09-30 19:57:08 +0100

    testing github page 6

--- src/index.js
+++ src/index.js
@@ -5,18 +5,11 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
-    document.getElementById('root')
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-    ,
-  </React.StrictMode>,
+    <App />
+  </React.StrictMode>
+);
 
-)
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
+reportWebVitals();
